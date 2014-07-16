6580986fda11aaec6099c2d16ba3de24cd78bc86
MattCordeiro <matthew3cord@gmail.com>
2014-07-16 16:39:44 -0700

    Work in progress on responsive header and fonts

--- themes/ne-digital/header.php
+++ themes/ne-digital/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
@@ -35,15 +35,24 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<!-- <img src="<?//php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img"> -->
 						</a>
 					</div>
 					<!-- /logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
+                        <a class="main-logo" href="<?php echo home_url();?>"><img src="<?php echo get_template_directory_uri(); ?>/img/nedc-logo.png"></a>
 						<?php html5blank_nav(); ?>
 					</nav>
+                    <div class="head-social">
+                        <ul>
+                            <li class="">fb</li>
+                            <li class="">li</li>
+                            <li class="">tw</li>
+                            <li class="">ho</li>
+                        </ul>
+                    </div>
 					<!-- /nav -->
 
 			</header>
