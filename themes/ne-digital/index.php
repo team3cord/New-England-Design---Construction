17e3f9159c5715b8185b43f39f1590ca06b2d4fe
MattCordeiro <matthew3cord@gmail.com>
2014-07-23 17:13:13 -0700

    Added custom fields to home page and images

--- themes/ne-digital/index.php
+++ themes/ne-digital/index.php
@@ -1,19 +1,30 @@
-<?php get_header(); ?>
+<?php  /* Template Name: HOME */ get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<div class="home-slide">
+    <?//php echo get_field('home_slider'); ?>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/kitchen-1.png">
+</div>
+<div class="home-drop">
+    <h1><?php echo get_field('drop_down_header'); ?></h1>
+    <a href="#"><?php echo get_field('drop_down_button'); ?></a>
+    <div class="drop-content">
+        <?php echo get_field('drop_down_content'); ?>
+    </div>
+</div>
+<aside class="home-sidebar">
+    <h2><?php echo get_field('sidebar_header'); ?></h2>
+    <div class="home-sidebar-content">
+        <?php echo get_field('sidebar_content'); ?>
+    </div>
+    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/guild-quality.png"></a>
+</aside>
+<aside class="left-call-today">
+    <h2><?php echo get_field('call_today_consultation'); ?></h2>
+    <a href="tel:<?php echo get_field('call_today_phone'); ?>"><?php echo get_field('call_today_phone'); ?></a>
+</aside>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+<h1>IM HOME!</h1>
 
-			<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
