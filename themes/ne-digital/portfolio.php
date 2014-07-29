22ceab5bffce7285131b1a603beca06d7de56d12
MattCordeiro <matthew3cord@gmail.com>
2014-07-28 22:33:12 -0700

    Added in and configured Nimble Portfolio

--- themes/ne-digital/portfolio.php
+++ themes/ne-digital/portfolio.php
@@ -1,45 +1,20 @@
 <?php /* Template Name: PORTFOLIO */ get_header(); ?>
+<main role="main">
+    <div class="page-bar">
+        <div class="page-header">
+            <h2><?php the_title(); ?></h2>
+        </div>
 
-    <main role="main">
-        <!-- section -->
-        <section>
+        <div class="sidebar-widget">
+            <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
+            <a class="view-all" href="<?php echo get_permalink(16); ?>">VIEW ALL</a>
+        </div>
+    </div>
+    <div class="portfolio-section">
+        <?php echo get_field('nimble_shortcode'); ?>
 
-            <h1><?php the_title(); ?></h1>
-
-            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-                <!-- article -->
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                    <?php the_content(); ?>
-
-                    <?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-                    <br class="clear">
-
-                    <?php edit_post_link(); ?>
-
-                </article>
-                <!-- /article -->
-
-            <?php endwhile; ?>
-
-            <?php else: ?>
-
-                <!-- article -->
-                <article>
-
-                    <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-                </article>
-                <!-- /article -->
-
-            <?php endif; ?>
-
-        </section>
-        <!-- /section -->
     </main>
 
-<?php get_sidebar(); ?>
 
+<?php get_template_part('sub', 'footer'); ?>
 <?php get_footer(); ?>
---
+++ themes/ne-digital/single-portfolio.php
@@ -0,0 +1,68 @@
+<?php get_header(); ?>
+
+<main role="main">
+    <div class="page-bar">
+        <div class="page-header">
+            <h2><?php the_title(); ?></h2>
+        </div>
+
+    </div>
+    <div class="main-section">
+
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+            <!-- article -->
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+
+                <?php the_content(); // Dynamic Content ?>
+
+
+
+            </article>
+            <!-- /article -->
+
+        <?php endwhile; ?>
+
+        <?php else: ?>
+
+            <!-- article -->
+            <article>
+
+                <h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+
+            </article>
+            <!-- /article -->
+
+        <?php endif; ?>
+
+    </div>
+    <aside class="right-sidebar">
+        <div class="nimble-description">
+            <h3><?php echo get_field('project_description_title'); ?></h3>
+            <span class="thick-line"></span><span class="thin-line"></span></br>
+            <div class="project-content">
+                <?php echo get_field('project_content'); ?>
+            </div>
+        </div>
+        <div class="single-pro right-call-today">
+            <h3><?php echo get_field('call_today_consultation', 4); ?></h3>
+            <span class="thick-line"></span><span class="thin-line"></span></br>
+            <div class="call-tout">
+                <?php echo get_field('call_today_tout', 16); ?>
+            </div>
+            <a class="orange-phone" href="tel:<?php echo get_field('call_today_phone', 4); ?>"><?php echo get_field('call_today_phone', 4); ?></a></br>
+            <a class="btn-contact" href="/contact"><?php echo get_field('contact_button_text', 8); ?></a>
+
+        </div>
+
+
+    </aside>
+    <div class="weird-line"></div>
+
+</main>
+
+
+<?php get_template_part('sub', 'footer'); ?>
+<?php get_footer(); ?>
